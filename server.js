d732b6098fabe21717610987d40bdc42fafc84b9
Glitch (arrastic) <none>
2020-09-26 14:26:57 +0000

    🌶🐡 Updated with Glitch

--- server.js
+++ server.js
@@ -4200,7 +4200,7 @@ const sockets = (() => {
           // Create and bind a body for the player host
           let body = new Entity(loc);
           body.protect();
-          body.define(Class.eventtester); // Start as a basic tank
+          body.define(Class.weakling); // Start as a basic tank
           body.name = name; // Define the name
           // Dev hax
           if (socket.key === "testl" || socket.key === "testk") {
